6be6010ff3ba8fd2cea78deb219adfdd30723f9d
dahyun_0_1 <changum0520@gmail.com>
2025-03-21 16:36:19 +0900

    4일차 C++ 공부,  inline

---
+++ Day04/cp08_inline.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+/* ¸ÞÅ©·Î */
+#define ADD(a, b)	#a "+" #b
+#define PI			3.14
+#define MSG(x, y, z)	x ## y ## z
+int main()
+{
+	printf("ADD(a, b): %s\n", ADD(10, 20));
+	printf("MSG(x, y, z): %s\n", MSG("macro+", "operator+", "test"));
+	return 0;
+}
---
+++ Day04/cp09_inline2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+/* 메크로는 전처리기가 처리하지만, inline 함수는 컴파일러가 처리*/
+#define SQUARE(x) ((x)*(x))
+
+inline int funct(int x)
+{
+	return x * x;
+}
+
+int main()
+{	
+	std::cout << SQUARE(2) << std::endl;
+	std::cout << funct(2) << std::endl;
+
+	return 0;
+}
